ca4d4370b928f63d2d357c93ccb55dc396274dea
Allamyrat0906 <allamyrat.annayev.2001@gmail.com>
2023-09-19 19:08:26 +0500

    python homework

---
+++ homework_1.py
@@ -0,0 +1,17 @@
+print("""
+Welcome to my account!!!!
+
+""")
+name = "Allamyrat"
+age = 22
+height = 180
+weight = 73
+phone_number = "+99362467943"
+country = "Turkmenistan"
+
+print("My name is " + name)
+print("I am " + str(age) + "  yesrd old")
+print("My height is " + str(height))
+print("My weight is", weight)
+print("My phone number is " + phone_number)
+print("I am from in", country)
